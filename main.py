d6dc4b40c010d149e5f7c4b17540226d16367eb0
YellowSkin22 <bruce.sirau.verweij@gmail.com>
2022-08-02 08:36:52 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 08:24:01 2022
+
+@author: bruce
+"""
+
+### --- Libraries --- ### 
+import streamlit as st
+import pandas as pd
+from datetime import date
+
+### --- Relevant URLS --- ### 
+# https://www.webfx.com/tools/emoji-cheat-sheet/
+
+
+
+### --- Streamlit App --- ###
+
+## -- Display -- ##
+st.set_page_config(layout='wide') #Set app to widescreen
+
+## -- Header -- ##
+
+
+
